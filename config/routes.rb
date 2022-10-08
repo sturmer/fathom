6e727dcaff51825c816b1bb8567fe183ef675cc8
Gianluca Ciccarelli <gian@ciccarelli.dev>
2022-10-08 17:30:19 +0300

    Add Devise
    Currently there may be a problem when signing up, as the Devise
    Registrations controller tries to redirect using user_url after sign-up.
    Apparently that named route is not found. The user however is created
    and can log in. Will fix later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "static_pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'static_pages/home'
 end
